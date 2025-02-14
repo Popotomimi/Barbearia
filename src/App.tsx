be14d696eca33b5ee3a3f89521e06070d32a82ac
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2025-02-14 16:40:04 -0300

    Update time and service

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // React router dom
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // React-Toastify
 import { ToastContainer } from "react-toastify";
@@ -10,6 +10,9 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Schedule from "./components/Schedule";
 
+// Pages
+import Admin from "./components/pages/Admin";
+
 function App() {
   const api: string = "https://backendbarbearia-6205.onrender.com/api";
 
@@ -17,7 +20,10 @@ function App() {
     <BrowserRouter>
       <Navbar />
       <ToastContainer autoClose={3000} position="top-center" />
-      <Schedule api={api} />
+      <Routes>
+        <Route path="/" element={<Schedule api={api} />} />
+        <Route path="/admin" element={<Admin api={api} />} />
+      </Routes>
       <Footer />
     </BrowserRouter>
   );
--- src/components/pages/Admin.tsx
+++ src/components/pages/Admin.tsx
@@ -1,32 +1,25 @@
 // Axios
 import axios from "axios";
-
 // Hooks
 import { FormEvent, useEffect, useRef, useState } from "react";
-
-// Message
+// Icons
+import { FiEdit } from "react-icons/fi";
+import { MdDelete } from "react-icons/md";
 import { toast } from "react-toastify";
 
 interface ScheduleProps {
   api: string;
 }
 
-const Schedule: React.FC<ScheduleProps> = ({ api }) => {
+const Admin: React.FC<ScheduleProps> = ({ api }) => {
   const [clientes, setClientes] = useState<any[]>([]);
   const [name, setName] = useState<string>("");
   const [date, setDate] = useState<string>("");
   const [time, setTime] = useState<string>("");
-  const [service, setService] = useState<string>("");
   const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(false);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [currentClientId, setCurrentClientId] = useState<string | null>(null);
   const formRef = useRef<HTMLDivElement>(null);
-
-  const services = [
-    { name: "Só sombrancelha (15min)", duration: 15 },
-    { name: "Só pésinho (10min)", duration: 10 },
-    { name: "Corte (40min)", duration: 40 },
-    { name: "Corte+sombrancelha (50min)", duration: 50 },
-    { name: "Corte+barba+sobrancelha (1h e 10min)", duration: 70 },
-  ];
 
   const fetchClientes = async () => {
     try {
@@ -45,12 +38,16 @@ const Schedule: React.FC<ScheduleProps> = ({ api }) => {
     setName("");
     setDate("");
     setTime("");
-    setService("");
+    setIsEditing(false);
+    setCurrentClientId(null);
   };
 
   const checkAvailability = () => {
     return clientes.some(
-      (cliente) => cliente.date === date && cliente.time === time
+      (cliente) =>
+        cliente.date === date &&
+        cliente.time === time &&
+        cliente._id !== currentClientId
     );
   };
 
@@ -61,20 +58,17 @@ const Schedule: React.FC<ScheduleProps> = ({ api }) => {
       return;
     }
 
-    const selectedService = services.find((srv) => srv.name === service);
-
-    const cliente = {
-      name,
-      date,
-      time,
-      service: selectedService?.name,
-      duration: selectedService?.duration,
-    };
+    const cliente = { name, date, time };
 
     try {
       setIsButtonDisabled(true);
-      await axios.post(`${api}/cliente`, cliente);
-      toast.success("Agendamento realizado com sucesso!");
+      if (isEditing && currentClientId) {
+        await axios.patch(`${api}/cliente/${currentClientId}`, cliente);
+        toast.success("Agendamento editado com sucesso!");
+      } else {
+        await axios.post(`${api}/cliente`, cliente);
+        toast.success("Agendamento realizado com sucesso!");
+      }
       resetForm();
       fetchClientes();
     } catch (error: any) {
@@ -92,15 +86,30 @@ const Schedule: React.FC<ScheduleProps> = ({ api }) => {
     }
   };
 
+  const deleteCliente = async (id: any) => {
+    try {
+      await axios.delete(`${api}/cliente/${id}`);
+      toast.success("Agendamento removido com sucesso");
+      fetchClientes();
+    } catch (error) {
+      toast.error("Tente novamente mais tarde");
+    }
+  };
+
+  const startEdit = (cliente: any) => {
+    setName(cliente.name);
+    setDate(cliente.date);
+    setTime(cliente.time);
+    setIsEditing(true);
+    setCurrentClientId(cliente._id);
+    formRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <div>
-      <div className="background"></div>
-      <div className="content">
-        <h1 className="first">Marque um horário</h1>
-      </div>
-      <div className="background"></div>
+      <h1 className="first">Admim Page</h1>
       <div className="make-schedule" ref={formRef}>
-        <h1>Faça seu Agendamento</h1>
+        <h1>{isEditing ? "Editar Agendamento" : "Faça seu Agendamento"}</h1>
         <form onSubmit={handleSubmit}>
           <div className="form-control">
             <label>Nome:</label>
@@ -130,51 +139,36 @@ const Schedule: React.FC<ScheduleProps> = ({ api }) => {
               onChange={(e) => setTime(e.target.value)}
             />
           </div>
-          <div className="form-control">
-            <label>Serviço:</label>
-            <select
-              value={service}
-              onChange={(e) => setService(e.target.value)}>
-              <option value="">Selecione um serviço</option>
-              {services.map((srv) => (
-                <option key={srv.name} value={srv.name}>
-                  {srv.name}
-                </option>
-              ))}
-            </select>
-          </div>
           {isButtonDisabled ? (
             <input type="submit" disabled value="Aguarde..." />
           ) : (
-            <input type="submit" value="Agendar" />
+            <input type="submit" value={isEditing ? "Editar" : "Agendar"} />
           )}
         </form>
       </div>
-      <img className="img" src="/img/tesoura.jpg" alt="Imagem de uma tesoura" />{" "}
       <div className="agenda-container">
-        {" "}
-        <h2>Agenda:</h2>{" "}
+        <h2>Agenda:</h2>
         {clientes.map((cliente) => (
           <div className="agenda" key={cliente._id}>
-            {" "}
             <div className="info">
-              {" "}
-              <h3>{cliente.name}</h3> <p>{cliente.date}</p>{" "}
-              <p>{cliente.time}</p>{" "}
-            </div>{" "}
-            <div className="actions"></div>{" "}
+              <h3>{cliente.name}</h3>
+              <p>{cliente.date}</p>
+              <p>{cliente.time}</p>
+            </div>
+            <div className="actions">
+              <label onClick={() => startEdit(cliente)}>Editar</label>
+              <FiEdit className="edit" onClick={() => startEdit(cliente)} />
+              <label onClick={() => deleteCliente(cliente._id)}>Remover</label>
+              <MdDelete
+                onClick={() => deleteCliente(cliente._id)}
+                className="delete"
+              />
+            </div>
           </div>
-        ))}{" "}
-      </div>{" "}
-      <div className="maps-container">
-        {" "}
-        <h1>Localização:</h1>{" "}
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1828.0976657697122!2d-46.782897044280496!3d-23.597326647604845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce55ab7ae71ff9%3A0xa44be7c84039c334!2sAv.%20Eng.%20Heitor%20Ant%C3%B4nio%20Eiras%20Garcia%2C%206064%20-%20Jardim%20Esmeralda%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2005564-200!5e0!3m2!1spt-BR!2sbr!4v1739475197774!5m2!1spt-BR!2sbr"
-          loading="lazy"></iframe>{" "}
+        ))}
       </div>
     </div>
   );
 };
 
-export default Schedule;
+export default Admin;
