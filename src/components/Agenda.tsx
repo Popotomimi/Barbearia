e86ccb5fc03cf8ce7e7fa462b7dda82273e6266c
Roberto de Oliveira <92410083+Popotomimi@users.noreply.github.com>
2025-04-05 18:27:36 -0300

    Update
    Update agenda, CSS and linguage

--- src/components/Agenda.tsx
+++ src/components/Agenda.tsx
@@ -1,22 +1,39 @@
+import { useState } from "react";
 import Cliente from "../interfaces/Cliente";
+import { FcCalendar } from "react-icons/fc";
 
 const Agenda = ({ clientes }: { clientes: Cliente[] }) => {
+  const [filter, setFilter] = useState<string | null>(null);
+
+  const filteredClientes = filter
+    ? clientes.filter((cliente) => cliente.barber === filter)
+    : clientes;
+
   const isLoading = clientes.length === 0;
 
   return (
     <div className="agenda-container">
       <h2>Agenda:</h2>
+      <p className="text-center">Filtre a agenda: </p>
+      <div className="filter-buttons">
+        <button onClick={() => setFilter("Gui")}>Gui</button>
+        <button onClick={() => setFilter("Gabriel")}>Gabriel</button>
+        <button onClick={() => setFilter(null)}>Todos</button>
+      </div>
       {isLoading ? (
         <div className="loading-container">
           <div className="loader"></div>
         </div>
       ) : (
         <div className="agenda-grid">
-          {clientes.map((cliente) => (
+          {filteredClientes.map((cliente) => (
             <div className="agenda-card" key={cliente._id}>
+              <div className="calendar-icon">
+                <FcCalendar />
+              </div>
               <div className="info">
                 <h3>{cliente.name}</h3>
-                <p>Data: {cliente.date}</p>
+                <p>Data: {cliente.date.split("-").reverse().join("/")}</p>
                 <p>Hora: {cliente.time}</p>
                 <p>Serviço: {cliente.service}</p>
                 <p>Barbeiro: {cliente.barber}</p>
--- src/components/AgendaAdmin.tsx
+++ src/components/AgendaAdmin.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { FiEdit } from "react-icons/fi";
 import { MdDelete } from "react-icons/md";
+import { FcCalendar } from "react-icons/fc";
 import AgendaAdminProps from "../interfaces/AgendaAdminProps";
 
 const AgendaAdmin = ({
@@ -8,36 +10,49 @@ const AgendaAdmin = ({
   deleteCliente,
   startEdit,
 }: AgendaAdminProps) => {
+  const [filter, setFilter] = useState<string | null>(null);
+
+  const filteredClientes = filter
+    ? clientes.filter((cliente) => cliente.barber === filter)
+    : clientes;
+
   return (
     <div>
       <div className="agenda-container">
         <h2>Agenda:</h2>
+        <p className="text-center">Filtre a agenda:</p>
+        <div className="filter-buttons">
+          <button onClick={() => setFilter("Gui")}>Gui</button>
+          <button onClick={() => setFilter("Gabriel")}>Gabriel</button>
+          <button onClick={() => setFilter(null)}>Todos</button>
+        </div>
         {isLoading ? (
           <div className="loading-container">
             <div className="loader"></div>
           </div>
         ) : (
           <div className="agenda-grid">
-            {clientes.map((cliente) => (
+            {filteredClientes.map((cliente) => (
               <div className="agenda-card" key={cliente._id}>
+                <div className="calendar-icon">
+                  <FcCalendar />
+                </div>
                 <div className="info">
                   <h3>{cliente.name}</h3>
-                  <p>{cliente.date}</p>
+                  <p>{cliente.date.split("-").reverse().join("/")}</p>
                   <p>{cliente.time}</p>
                   <p>{cliente.service}</p>
                   <p>{cliente.barber}</p>
                   <p>{cliente.phone}</p>
                 </div>
                 <div className="actions">
-                  <label onClick={() => startEdit(cliente)}>Editar</label>
-                  <FiEdit className="edit" onClick={() => startEdit(cliente)} />
+                  <label onClick={() => startEdit(cliente)}>
+                    Editar <FiEdit className="edit" />
+                  </label>
                   <label onClick={() => deleteCliente(cliente._id)}>
                     Remover
+                    <MdDelete className="delete" />
                   </label>
-                  <MdDelete
-                    onClick={() => deleteCliente(cliente._id)}
-                    className="delete"
-                  />
                 </div>
               </div>
             ))}
